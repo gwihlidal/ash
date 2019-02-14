8dee16ec2c6f685a1d629b87fac5b3ebc9f4c60a
Graham Wihlidal <graham@wihlidal.ca>
2019-02-14 23:33:41 +0100

    Added disassembly script and results

---
+++ examples/shader/nv_ray_tracing/dis.bat
@@ -0,0 +1,7 @@
+spirv-dis.exe -o triangle.hlsl_rgen.txt triangle.hlsl_rgen.spv
+spirv-dis.exe -o triangle.glsl_rgen.txt triangle.glsl_rgen.spv
+spirv-dis.exe -o triangle.hlsl_rchit.txt triangle.hlsl_rchit.spv
+spirv-dis.exe -o triangle.glsl_rchit.txt triangle.glsl_rchit.spv
+spirv-dis.exe -o triangle.hlsl_rmiss.txt triangle.hlsl_rmiss.spv
+spirv-dis.exe -o triangle.glsl_rmiss.txt triangle.glsl_rmiss.spv
+spirv-dis.exe -o triangle.hlsl_lib.txt triangle.hlsl_lib.spv
