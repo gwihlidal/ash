6a893670fc949ce6217341b7eb5e634adf99614a
Graham Wihlidal <graham@wihlidal.ca>
2019-02-14 18:49:30 +0100

    Temp windows build script

---
+++ examples/shader/nv_ray_tracing/compile_hlsl.bat
@@ -0,0 +1,2 @@
+docker run --rm -it -v "//h/Repositories/ash/examples/shader/nv_ray_tracing":"//h/Repositories/ash/examples/shader/nv_ray_tracing" -w "//h/Repositories/ash/examples/shader/nv_ray_tracing" gwihlidal/docker-shader:vk_rt
+
